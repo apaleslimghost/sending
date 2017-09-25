00e8e80ca2f14671c51bbdc51bdd93df47a854a2
Matt Brennan <matt@153.io>
2017-09-25 14:39:28 +0100

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+const {send, json} = require('micro');
+const getUrls = require('get-urls');
+const fetch = require('node-fetch');
+
+const {WEBHOOK_URL: webhookUrl} = process.env;
+
+console.log(webhookUrl);
+
+module.exports = async (req, res) => {
+	const {subject, body} = await json(req);
+	const text = subject.replace(/^\[The Broken Crown\] /, '');
+	const [url = 'http://thebrokencrown.obsidianportal.com'] = getUrls(body);
+
+	const r = await fetch(webhookUrl, {
+		method: 'POST',
+		headers: {'content-type': 'application/json'},
+		body: JSON.stringify({text, url}),
+	});
+
+	send(res, r.status, await r.json());
+};
